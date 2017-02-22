b1fad905a94654b464a4a1fafa542df9db87cb6c
avula12345 <ramamohan.avula@gmail.com>
2017-02-22 22:05:27 +0530

    Added two new java files

---
+++ src/test/java/Org/Project/SpiceJet_Test/GetTitle.java
@@ -0,0 +1,9 @@
+package Org.Project.SpiceJet_Test;
+
+public class GetTitle {
+
+	public static void main(String[] args) {
+			System.out.println("Title");
+	}
+
+}
---
+++ src/test/java/Org/Project/SpiceJet_Test/printname.java
@@ -0,0 +1,10 @@
+package Org.Project.SpiceJet_Test;
+
+public class printname {
+
+	public static void main(String[] args) {
+		System.out.println("vishnu");
+
+	}
+
+}
