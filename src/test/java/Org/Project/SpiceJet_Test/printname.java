5812040081ce9b703dcf1a50c8a9cccc3f8d974b
vishnuqa23 <vishnu.qa23@gmail.com>
2017-08-18 03:19:43 +0530

    Update printname.java

--- src/test/java/Org/Project/SpiceJet_Test/printname.java
+++ src/test/java/Org/Project/SpiceJet_Test/printname.java
@@ -14,6 +14,7 @@ public class printname {
 		System.out.println("ZZZZZZZZZZZZZZZZZZZ");
 		System.out.println("QQQQQQQQQQQQQQQQQQQQQQQQQQQQQ");
 		System.out.println("ZXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
 	}
 
 }
